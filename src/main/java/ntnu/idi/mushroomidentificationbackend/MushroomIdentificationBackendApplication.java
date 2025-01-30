6b1f9183a15253435445ad5d2dcc4573c3767eec
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-01-30 11:13:19 +0100

    Initial commit

---
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/MushroomIdentificationBackendApplication.java
@@ -0,0 +1,13 @@
+package ntnu.idi.mushroomidentificationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MushroomIdentificationBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MushroomIdentificationBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ntnu/idi/mushroomidentificationbackend/MushroomIdentificationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ntnu.idi.mushroomidentificationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MushroomIdentificationBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
