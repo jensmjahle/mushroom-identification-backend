a50daf3ee5a2f5c514cc53475f29625d37f1507e
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-02-01 17:19:16 +0100

    Add repository methods

--- src/main/java/ntnu/idi/mushroomidentificationbackend/repository/MessageRepository.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/repository/MessageRepository.java
@@ -1,9 +1,13 @@
 package ntnu.idi.mushroomidentificationbackend.repository;
 
+import java.util.List;
 import ntnu.idi.mushroomidentificationbackend.model.entity.Message;
+import ntnu.idi.mushroomidentificationbackend.model.entity.UserRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, String> {
+  // Retrieve all messages associated with a specific UserRequest
+  List<Message> findByUserRequest(UserRequest userRequest);
 }
--- src/main/java/ntnu/idi/mushroomidentificationbackend/repository/UserRequestRepository.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/repository/UserRequestRepository.java
@@ -1,9 +1,20 @@
 package ntnu.idi.mushroomidentificationbackend.repository;
 
+import ntnu.idi.mushroomidentificationbackend.model.entity.Admin;
 import ntnu.idi.mushroomidentificationbackend.model.entity.UserRequest;
+import ntnu.idi.mushroomidentificationbackend.model.enums.UserRequestStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRequestRepository extends JpaRepository<UserRequest, String> {
+
+  // Get paginated user requests by status sorted by updatedAt (ascending)
+  Page<UserRequest> findByStatusOrderByUpdatedAtAsc(UserRequestStatus status, Pageable pageable);
+  
+  // Get paginated user requests for a specific admin by status, sorted by updatedAt (ascending)
+  Page<UserRequest> findByAdminAndStatusOrderByUpdatedAtAsc(Admin admin, UserRequestStatus status, Pageable pageable);
+
 }
