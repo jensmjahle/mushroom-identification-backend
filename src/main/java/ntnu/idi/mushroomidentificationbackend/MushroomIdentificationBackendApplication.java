da526e5da3692bfa6902f02cf929df82aa2bc4db
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-02-06 10:08:50 +0100

    pipeline fix

--- src/main/java/ntnu/idi/mushroomidentificationbackend/MushroomIdentificationBackendApplication.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/MushroomIdentificationBackendApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MushroomIdentificationBackendApplication {
   static {
-    Dotenv dotenv = Dotenv.load(); // Load .env file
-    System.setProperty("DB_URL", dotenv.get("DB_URL"));
-    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    if (!"dev".equals(System.getProperty("SPRING_PROFILES_ACTIVE"))) {
+      Dotenv dotenv = Dotenv.load(); // Load .env file
+      System.setProperty("DB_URL", dotenv.get("DB_URL"));
+      System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+      System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+    }
   }
 
   public static void main(String[] args) {
