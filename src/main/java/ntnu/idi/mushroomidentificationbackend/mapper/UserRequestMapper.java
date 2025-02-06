7924696bd0645ec897d1bdedad3954a344974e3a
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-02-06 16:24:52 +0100

    create UserRequestMapper

---
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/mapper/UserRequestMapper.java
@@ -0,0 +1,28 @@
+package ntnu.idi.mushroomidentificationbackend.mapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import ntnu.idi.mushroomidentificationbackend.dto.request.NewUserRequestDTO;
+import ntnu.idi.mushroomidentificationbackend.dto.response.RetrieveRequestAnswerDTO;
+import ntnu.idi.mushroomidentificationbackend.dto.response.message.MessageDTO;
+import ntnu.idi.mushroomidentificationbackend.model.entity.Message;
+import ntnu.idi.mushroomidentificationbackend.model.entity.UserRequest;
+
+public class UserRequestMapper {
+  private RetrieveRequestAnswerDTO fromEntityToDto(UserRequest userRequest, List<Message> messages)
+      throws IOException {
+    List<MessageDTO> messageDTOs = new ArrayList<>();
+    for (Message message : messages) {
+      messageDTOs.add(MessageMapper.fromEntityToDto(message));
+    }
+    return new RetrieveRequestAnswerDTO(
+      userRequest.getReferenceCode(),
+      userRequest.getCreatedAt(),
+      userRequest.getUpdatedAt(),
+      userRequest.getStatus(),
+      messageDTOs
+    );
+  }
+
+}
