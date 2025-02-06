a0cb000ed7b940eb3a9dcc8ebd9d278f198be8ce
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-02-06 12:29:05 +0100

    pipeline fix

--- src/main/java/ntnu/idi/mushroomidentificationbackend/MushroomIdentificationBackendApplication.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/MushroomIdentificationBackendApplication.java
@@ -1,22 +1,33 @@
 package ntnu.idi.mushroomidentificationbackend;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import java.io.File;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class MushroomIdentificationBackendApplication {
+  @Value("${spring.profiles.active}")
+  private static String activeProfile;
+
   static {
-    if (!"dev".equals(System.getProperty("SPRING_PROFILES_ACTIVE"))) {
+    
+    if (new File(".env").exists()) {
+      System.out.println("Loading .env file");
       Dotenv dotenv = Dotenv.load(); // Load .env file
       System.setProperty("DB_URL", dotenv.get("DB_URL"));
       System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
       System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+    } else {
+      System.out.println("No .env file found, using default values");
     }
   }
 
   public static void main(String[] args) {
     SpringApplication.run(MushroomIdentificationBackendApplication.class, args);
   }
-
 }
--- src/main/java/ntnu/idi/mushroomidentificationbackend/security/SecurityConfig.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package ntnu.idi.mushroomidentificationbackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
