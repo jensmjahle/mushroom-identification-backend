719aa0ae510c2adf8d4be44a5e17be0505563086
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-17 11:13:45 +0100

    Add private constructor and javadoc to UserRequestMapper

--- src/main/java/ntnu/idi/mushroomidentificationbackend/mapper/UserRequestMapper.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/mapper/UserRequestMapper.java
@@ -10,6 +10,22 @@ import ntnu.idi.mushroomidentificationbackend.model.entity.Message;
 import ntnu.idi.mushroomidentificationbackend.model.entity.UserRequest;
 
 public class UserRequestMapper {
+
+  /**
+   * Private constructor to prevent instantiation. Throws UnsupportedOperationException if it's instantiated.
+   */
+  private UserRequestMapper() {
+    throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+  }
+
+  /**
+   * Maps a UserRequest entity to a DTO including the request's messages.
+   *
+   * @param userRequest User requests entity. 
+   * @param messages All messages associated with the request.
+   * @return An UserRequestWithMessages DTO.
+   * @throws IOException If the image-messages are not retrievable from the local storage. 
+   */
   public static UserRequestWithMessagesDTO fromEntityToDto(UserRequest userRequest, List<Message> messages)
       throws IOException {
     List<MessageDTO> messageDTOs = new ArrayList<>();
@@ -25,7 +41,13 @@ public class UserRequestMapper {
       messageDTOs
     );
   }
-  
+
+  /**
+   * Maps a UserRequest entity to a DTO without the request's message.
+   *
+   * @param userRequest User requests entity.
+   * @return |UserRequestWithoutMessagesDTO.
+   */
   public static UserRequestWithoutMessagesDTO fromEntityToDto(UserRequest userRequest) {
     return new UserRequestWithoutMessagesDTO(
         userRequest.getReferenceCode(),
