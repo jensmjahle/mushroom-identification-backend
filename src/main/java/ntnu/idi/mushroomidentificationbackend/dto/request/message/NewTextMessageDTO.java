3db7ff13d12ed64bfff78722315de4f1b41b8f4f
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-17 15:57:30 +0100

    refactor messages dtos

--- src/main/java/ntnu/idi/mushroomidentificationbackend/dto/request/message/NewTextMessageDTO.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/dto/request/message/NewTextMessageDTO.java
@@ -1,21 +1,20 @@
-package ntnu.idi.mushroomidentificationbackend.dto.response.message;
+package ntnu.idi.mushroomidentificationbackend.dto.request.message;
 
 import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ntnu.idi.mushroomidentificationbackend.dto.response.message.MessageDTO;
 import ntnu.idi.mushroomidentificationbackend.model.enums.MessageSenderType;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class TextMessageDTO extends MessageDTO {
+public class NewTextMessageDTO extends NewMessageDTO {
   private String text;
 
-  public TextMessageDTO(MessageSenderType senderType, Date createdAt, String content) {
+  public NewTextMessageDTO(MessageSenderType senderType, Date createdAt, String content) {
     super(senderType, createdAt);
     this.text = content;
   }
---
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/dto/response/MessageDTO.java
@@ -0,0 +1,18 @@
+package ntnu.idi.mushroomidentificationbackend.dto.response;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import ntnu.idi.mushroomidentificationbackend.model.enums.MessageSenderType;
+import ntnu.idi.mushroomidentificationbackend.model.enums.MessageType;
+@AllArgsConstructor
+@Getter
+@Setter
+public class MessageDTO {
+  private String messageId;
+  private MessageSenderType senderType;
+  private MessageType messageType;
+  private String content;
+  private Date createdAt;
+}
