ae89215b539fc9e10923fe22385fd457495f7850
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-19 13:17:19 +0100

    add changeRequestStatus method in UserRequestService

---
+++ src/test/java/ntnu/idi/mushroomidentificationbackend/service/UserRequestServiceTest.java
@@ -0,0 +1,35 @@
+package ntnu.idi.mushroomidentificationbackend.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+import ntnu.idi.mushroomidentificationbackend.dto.request.ChangeRequestStatusDTO;
+import ntnu.idi.mushroomidentificationbackend.model.entity.UserRequest;
+import ntnu.idi.mushroomidentificationbackend.model.enums.UserRequestStatus;
+import ntnu.idi.mushroomidentificationbackend.repository.UserRequestRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class UserRequestServiceTest {
+
+  @Autowired
+  private UserRequestService userRequestService;
+
+  @Autowired
+  private UserRequestRepository userRequestRepository;
+
+
+  @Test
+  void changeRequestStatus_throwsExceptionIfUserRequestNotFound() {
+    ChangeRequestStatusDTO dto = new ChangeRequestStatusDTO();
+    dto.setUserRequestId("99999"); 
+    dto.setNewStatus(UserRequestStatus.PENDING);
+
+    Exception exception = assertThrows(RuntimeException.class, () -> userRequestService.changeRequestStatus(dto));
+    assertTrue(exception.getMessage().contains("User request not found"));
+  }
+}
