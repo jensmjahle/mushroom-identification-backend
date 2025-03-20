ebd5e041913161f18f4b9169126b49dd12fd6cad
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-20 16:07:03 +0100

    move cors allowed origins to application.properties

--- src/main/java/ntnu/idi/mushroomidentificationbackend/config/CorsConfig.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package ntnu.idi.mushroomidentificationbackend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,10 +12,13 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+  @Value("${app.cors.allowed-origins}")
+  private String allowedOrigins;
+
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173")); 
+    configuration.setAllowedOrigins(List.of(allowedOrigins.split(",")));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
     configuration.setAllowCredentials(true);
@@ -24,4 +28,3 @@ public class CorsConfig {
     return source;
   }
 }
-
