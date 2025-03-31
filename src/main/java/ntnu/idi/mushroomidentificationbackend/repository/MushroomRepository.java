a3ea8b466de8ed5f49fb9c67f574c530bdc9e4eb
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-31 11:27:08 +0200

    add methods in MushroomMapper and MushroomRepository

--- src/main/java/ntnu/idi/mushroomidentificationbackend/repository/MushroomRepository.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/repository/MushroomRepository.java
@@ -1,11 +1,15 @@
 package ntnu.idi.mushroomidentificationbackend.repository;
 
+import java.util.List;
+import java.util.Optional;
 import ntnu.idi.mushroomidentificationbackend.model.entity.Mushroom;
+import ntnu.idi.mushroomidentificationbackend.model.entity.UserRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MushroomRepository extends JpaRepository<Mushroom, String> {
-  
 
+
+  List<Mushroom> findByUserRequest(Optional<UserRequest> userRequest);
 }
