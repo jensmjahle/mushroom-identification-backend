6e4805d42b8c0242cd2c928ad02ea3861c39b5a5
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-04-24 15:31:28 +0200

    add profile update and password change validation in AdminService

--- src/main/java/ntnu/idi/mushroomidentificationbackend/service/AdminService.java
+++ src/main/java/ntnu/idi/mushroomidentificationbackend/service/AdminService.java
@@ -60,6 +60,14 @@ public AdminService(AdminRepository adminRepository, PasswordEncoder passwordEnc
         .map(AdminMapper::fromEntityToDto);
   }
 
+  /**
+   * Update the profile of an admin.
+   * Checks if the admin exists and
+   * if the new email is valid and not taken.
+   *
+   * @param request the request containing the new profile information
+   * @param username the username of the admin
+   */
   @Transactional
   public void updateProfile(UpdateProfileDTO request, String username) {
     Optional<Admin> adminOptional = adminRepository.findByUsername(username);
@@ -67,6 +75,12 @@ public AdminService(AdminRepository adminRepository, PasswordEncoder passwordEnc
       Admin admin = adminOptional.get();
       admin.setFirstname(request.getFirstname());
       admin.setLastname(request.getLastname());
+      if (adminRepository.findByEmail(request.getEmail()).isPresent()) {
+        throw new UsernameAlreadyExistsException("Email '" + request.getEmail() + "' is already taken.");
+      }
+      if (!request.getEmail().matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+        throw new IllegalArgumentException("Invalid email format");
+      }
       admin.setEmail(request.getEmail());
       adminRepository.save(admin);
     } else {
@@ -74,18 +88,49 @@ public AdminService(AdminRepository adminRepository, PasswordEncoder passwordEnc
     }
   }
 
+  /**
+   * Change the password of an admin.
+   * Checks if the old password is correct,
+   * the new password meets the criteria,
+   * if the admin exists and 
+   * if the new password and confirm password match.
+   * The new password must be at least eight characters long,
+   * at most 20 characters long,
+   * cannot contain spaces,
+   * must contain at least one number,
+   * and at least one upper case letter.
+   *
+   * @param request the request containing the old, new, and confirm password
+   * @param username the username of the admin
+   */
   public void changePassword(ChangePasswordDTO request, String username) {
     Optional<Admin> adminOptional = adminRepository.findByUsername(username);
-    if (adminOptional.isPresent()) {
-      Admin admin = adminOptional.get();
-      if (passwordEncoder.matches(request.getOldPassword(), admin.getPasswordHash())) {
-        admin.setPasswordHash(passwordEncoder.encode(request.getNewPassword()));
-        adminRepository.save(admin);
-      } else {
-        throw new UnauthorizedAccessException("Old password is incorrect");
-      }
-    } else {
+    if (adminOptional.isEmpty()) {
       throw new UnauthorizedAccessException("Admin not found");
     }
+    Admin admin = adminOptional.get();
+    if (!passwordEncoder.matches(request.getOldPassword(), admin.getPasswordHash())) {
+      throw new UnauthorizedAccessException("Old password is incorrect");
+    }
+    if(request.getNewPassword().length() < 8) {
+      throw new IllegalArgumentException("New password must be at least 8 characters long");
+    }
+    if(request.getNewPassword().length() > 20) {
+      throw new IllegalArgumentException("New password must be at most 20 characters long");
+    }
+    if(request.getNewPassword().contains(" ")) {
+      throw new IllegalArgumentException("New password cannot contain spaces");
+    }
+    if(!request.getNewPassword().matches(".*\\d.*")) {
+      throw new IllegalArgumentException("New password must contain at least one number");
+    }
+    if(!request.getNewPassword().matches(".*[A-Z].*")) {
+      throw new IllegalArgumentException("New password must contain at least one upper case letter");
+    }
+    if (!request.getNewPassword().equals(request.getConfirmPassword())) {
+      throw new IllegalArgumentException("New password and confirm password do not match");
+    }
+    admin.setPasswordHash(passwordEncoder.encode(request.getNewPassword()));
+    adminRepository.save(admin);
   }
 }
